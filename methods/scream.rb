77b983ab84dbebda20bbcc90c9a2973f6a9ba257
Lawrence Gladnick <larry.gladnick@gmail.com>
2017-03-31 21:50:53 -0400

    screaming

---
+++ methods/scream.rb
@@ -0,0 +1,10 @@
+### this code will print nothing because it returns before the puts
+##  code below shows results to question 5
+
+def scream(words)
+  words = words + "!!!!"
+ 
+  puts words
+end
+
+scream("Yippeee")
